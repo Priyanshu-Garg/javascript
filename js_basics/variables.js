aa87a78e5a6a0cc751051c3809fb8a7552767304
Priyanshu-Garg <88273420+Priyanshu-Garg@users.noreply.github.com>
2023-07-26 09:25:53 +0000

    variables and datatypes

---
+++ js_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId =12345
+let accountEmail="priyanshu@gmail.com"
+var accountPassword="9876"
+accountCity="Delhi"
+
+// accountId=7
+accountEmail="pg@gmail.com"
+accountPassword="567"
+accountCity="hapur"
+let accountState;
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
