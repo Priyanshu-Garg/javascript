6306f4afa1be664f109ad507eb570c6c5fa74a68
Priyanshu-Garg <88273420+Priyanshu-Garg@users.noreply.github.com>
2023-07-27 10:55:17 +0000

    datatypes

--- js_basics/dataTypes.js
+++ js_basics/dataTypes.js
@@ -9,3 +9,14 @@ console.log(id===id1)
 
 let big=11111111111111111111111111111111111n
 console.log(typeof big);
+
+let userOne={
+    name:"priyanshu",
+    age:19
+
+}
+
+let userTwo=userOne
+userTwo.age=20
+console.log(userTwo);
+console.log(userOne);
