f4ef11b2523c3810280dc09c49323d77910e3839
Priyanshu-Garg <88273420+Priyanshu-Garg@users.noreply.github.com>
2023-07-28 12:20:48 +0000

    array

---
+++ 02_basics/array.js
@@ -0,0 +1,10 @@
+
+const marvel=['tony','hulk','thor'];
+const dc=['superman','flash']
+const all=marvel.concat(dc)
+console.log(all)
+
+const nums=[1,2,3,[2,34,6,[89,7]],8]
+console.log(nums);
+const nums1=nums.flat(1);
+console.log(nums1);
